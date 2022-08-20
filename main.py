8a52bd55a16b65e4ab44f8a32bc20a4d15f0d3cb
finnhewes <finnhewes@gmail.com>
2022-08-20 21:21:56 +0300

    added master password feature
    created a new feature. on startup, the program checks for a file named "master.key". if found, the file is read, and the password is assigned to the variable "saved_master_password". if not found, a pop-up window prompts us to set a master password.
    
    Also: 
    added some useful comments, 
    rearranged some things for clarity,
    changed my actual email out for an example email,
    renamed the program window title "Offline Password Manager" vs the previous, "Password Manager" ;)

--- main.py
+++ main.py
@@ -27,35 +27,35 @@ def data_search():
     if len(website) <= 1:
         messagebox.showerror(title="Whoops!", message="Please enter a website name and try again.")
     else:
-        #   tries to pull up our previously saved password
-        try:
+        
+        try:                    #   try to retrieve our previously saved password data
             open("data.json")
-        #   if the saved password file CANNOT be found, the user is prompted to start saving some passwords before
-        #   trying to search for passwords
+        #   if the saved password data CANNOT be found, the user is prompted to start saving some passwords
         except FileNotFoundError:
             messagebox.showerror(title="Whoops! File not found.", message="No data file exists yet! Start by saving "
                                                                           "some logins and passwords!")
         #   if the saved password file CAN be found, the program will open our data json file
         else:
             with open("data.json", mode="r") as data_file:
-                # reading saved data
-                data = json.load(data_file)
-                #   the program will try to find saved username and password data for the given website input
-                try:
+                data = json.load(data_file)     
+                try:    #   the program will try to find saved username and password data for the given website input
                     website_data = data[website]
-                #   if this data CANNOT be found, the user is prompted to generate a new password for this site instead.
-                except KeyError:
+                    
+                except KeyError:    #   if this data CANNOT be found, the user is prompted to generate a new password for this site instead.
                     messagebox.showerror(title="Whoops! Data not found.",
                                          message="No details for this website exists yet! \n"
                                                  "Try generating a new password for this site instead.")
-                #   if this data IS found, the program makes a popup window populated with the data we're searching for:
-                #  the previously saved username and password for a given website input
-                else:
-                    email_data = website_data["email"]
-                    password_data = website_data["password"]
-                    messagebox.showinfo(title=website_data, message=f"Login: {email_data}\nPassword:{password_data}")
-                    web_ent.delete(0, END)
-
+                
+                else:   #   if this data IS found, the program will request a master password to authenticate the user
+                    master_password = simpledialog.askstring('Input Master Password',
+                                                             'Please enter your master password: ')
+                    if master_password == saved_master_password:        # check password input against stored password
+                        # if we pass this authentication step, the requested data will be displayed
+                        email_data = website_data["email"]
+                        password_data = website_data["password"]
+                        messagebox.showinfo(title=website_data, message=f"Login: {email_data}\nPassword:{password_data}")
+                        # now, we'll clear the website entry section of the window to "reset" the gui for future interactions.
+                        web_ent.delete(0, END)  
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 # saves our user inputted website and user inputted password, OR the password we randomly generated, to our json file
@@ -107,11 +107,11 @@ def save_entry():
 
 #   General setup
 window = Tk()
-window.title("Password Manager")
+window.title("Offline Password Manager")
 window.config(width=200, height=200, padx=50, pady=50)
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 
-#   Everybody's gotta have branding! Some beautiful, customized pixel art will do for me.
+#   Pretty logo
 image = PhotoImage(file=IMG)
 canvas.create_image(100, 100, image=image)
 canvas.grid(row=0, column=1)
@@ -127,7 +127,7 @@ em_lab = Label(text="E-Mail/Username:", highlightthickness=0)
 em_lab.grid(row=2, column=0)
 em_ent = Entry(width=36, highlightthickness=0)
 #   Adds a default email to the email input. Swap this for your own email to make life easier!
-em_ent.insert(0, "finnhewes@gmail.com")
+em_ent.insert(0, "user@example.com")
 em_ent.grid(row=2, column=1, columnspan=2)
 
 pass_lab = Label(text="Password:", highlightthickness=0)
@@ -146,5 +146,15 @@ search_button.grid(row=1, column=2)
 add_but = Button(text="Add", width=34, borderwidth=0, command=save_entry)
 add_but.grid(row=4, column=1, columnspan=2)
 
-# keeps our window open until exited, so that we can successfully interact with our program
-window.mainloop()
+# this is the master.key checking feature, allowing us to hide our saved data behind a "master password" protective layer
+try:        # tries to open the master.key file
+    with open('master.key', 'r') as master_file:
+        saved_master_password = master_file.read()      # if successful, the file is read and its contents are assigned to saved_master_password var.
+except FileNotFoundError:                               # if unsuccessful, a new file is created, and written based on input from Welcome pop-up window
+        with open('master.key', 'wb') as master_file:
+        master_password_provided = simpledialog.askstring('Welcome :)', "Please set a master password.\n"
+                                                                 "This will be what you use to access all of your "
+                                                                 "other passwords, so don't forget it!")
+        master_file.write(master_password_provided)
+        
+window.mainloop()       # keeps our window open until exited, so that we can successfully interact with our program
