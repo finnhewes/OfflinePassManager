8facf25175c7fa3a8af9a0b0d234b3360f4d9f9b
finnhewes <finnhewes@gmail.com>
2022-08-21 18:49:46 +0300

    added encryption master pass
    Added sha256 salted encryption layer to the master password verification layer.
    
    The master password is no longer stored in plaintext. The salt is stored in the salt.key file, and the encrypted, salted password is stored in the master.key file.
    
    When the program runs, it checks for the existence of the salt.key and master.key files, and if they don't exist, they're created.
    
    When the a password is requested and found in the data file, a popup window requests the master password. 
    This password is salted, and then encrypted and checked against the master.key file in storage. 
    
    If there's a match, the password data is displayed in a subsequent popup window. 
    
    The next update will add an encryption layer to the stored password data in our .json file.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from tkinter import *
 import secrets
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from tkinter import messagebox, simpledialog
 import pyperclip
 import json
@@ -22,14 +26,14 @@ def generate():
 #   Searches our generated text file for json data, and handles exceptions...
 def data_search():
     website = web_ent.get()
-    #   catches an invalid website input
+    #   rejects an invalid website input
     if len(website) <= 1:
         messagebox.showerror(title="Whoops!", message="Please enter a website name and try again.")
     else:
-
-        try:  # try to retrieve our previously saved password data
+        #   tries to retrieve the previously saved password
+        try:
             open("data.json")
-        #   if the saved password data CANNOT be found, the user is prompted to start saving some passwords
+        #   if the saved password file CANNOT be found, the user is prompted to start saving some passwords
         except FileNotFoundError:
             messagebox.showerror(title="Whoops! File not found.", message="No data file exists yet! Start by saving "
                                                                           "some logins and passwords!")
@@ -37,29 +41,45 @@ def data_search():
         else:
             with open("data.json", mode="r") as data_file:
                 data = json.load(data_file)
-                try:  # the program will try to find saved username and password data for the given website input
+                #   the program will try to find saved username and password data for the given website input
+                try:
                     website_data = data[website]
-
-                except KeyError:  # if this data CANNOT be found. user is prompted to generate a new password instead.
+                #   if this data CANNOT be found, the user is prompted to generate a new password for this site instead.
+                except KeyError:
                     messagebox.showerror(title="Whoops! Data not found.",
                                          message="No details for this website exists yet! \n"
                                                  "Try generating a new password for this site instead.")
-
-                else:  # if this data IS found, the program will request a master password to authenticate the user
+                #   if this data IS found, the program makes a popup window requesting the master password. if  entered
+                #   correctly, the program makes another popup window populated with the saved website login data
+                else:
                     master_password = simpledialog.askstring('Input Master Password',
                                                              'Please enter your master password: ')
-                    if master_password == saved_master_password:  # check password input against stored password
-                        # if we pass this authentication step, the requested data will be displayed
-                        email_data = website_data["email"]
-                        password_data = website_data["password"]
-                        messagebox.showinfo(title=website_data,
-                                            message=f"Login: {email_data}\nPassword:{password_data}")
-                        # now, we'll clear the website entry section of the window to "reset" the gui
-                        web_ent.delete(0, END)
+                    master_password_encoded = master_password.encode()  # convert entered pass into bytes
 
-                    # ---------------------------- SAVE PASSWORD ------------------------------- #
+                    with open('salt.key', 'rb') as s:
+                        salt = s.read()
+
+                    kdf = PBKDF2HMAC(
+                            algorithm=hashes.SHA256(),
+                            length=32,
+                            salt=salt,
+                            iterations=100000,
+                            backend=default_backend(),
+                        )
+                    generated_key = base64.urlsafe_b64encode(kdf.derive(master_password_encoded))  # stores our new key
+
+                    with open('master.key', 'r') as f:      # opens saved key file
+                        saved_key = f.read().encode()       # compared new key to saved key on file
+                        if generated_key == saved_key:
+                            email_data = website_data["email"]
+                            password_data = website_data["password"]
+                            # if we've made it this far, then we've authenticated, and we can display the login info.
+                            messagebox.showinfo(title=website_data, message=f"Login: {email_data}\n"
+                                                                            f"Password:{password_data}")
+                            web_ent.delete(0, END)  # clears the website field, effectively "resetting" our gui window
 
 
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 # saves our user inputted website and user inputted password, OR the password we randomly generated, to our json file
 def save_entry():
     website = web_ent.get()
@@ -98,7 +118,7 @@ def save_entry():
             with open("data.json", mode="w") as data_file:
                 # saving updated data into dictionary
                 json.dump(data, data_file, indent=4)
-        # finally, we will clear our input fields, and await a new password search or input.
+        # finally, we will clear our input fields, effectively "resetting" our gui window and await a new input.
         finally:
             web_ent.delete(0, END)
             pass_ent.delete(0, END)
@@ -109,11 +129,11 @@ def save_entry():
 
 #   General setup
 window = Tk()
-window.title("Offline Password Manager")
+window.title("Password Manager")
 window.config(width=200, height=200, padx=50, pady=50)
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 
-#   Pretty logo
+#   Everybody's gotta have branding! Some beautiful, customized pixel art will do for me.
 image = PhotoImage(file=IMG)
 canvas.create_image(100, 100, image=image)
 canvas.grid(row=0, column=1)
@@ -137,6 +157,7 @@ pass_lab.grid(row=3, column=0)
 pass_ent = Entry(width=21, highlightthickness=0)
 pass_ent.grid(row=3, column=1)
 
+
 #   Making Buttons, linking them to functions defined above.
 gen_but = Button(text="Generate Password", width=10, borderwidth=0, command=generate)
 gen_but.grid(row=3, column=2)
@@ -147,16 +168,31 @@ search_button.grid(row=1, column=2)
 add_but = Button(text="Add", width=34, borderwidth=0, command=save_entry)
 add_but.grid(row=4, column=1, columnspan=2)
 
-# this is the master.key checking feature allowing us to hide our saved data behind a "master password" protective layer
-try:  # tries to open the master.key file
-    with open('master.key', 'r') as master_file:    # if successful, then the file exists.
-        saved_master_password = master_file.read()  # the file's contents are assigned to saved_master_password var.
-except FileNotFoundError:  # if unsuccessful, a new file is created and written using input from "Welcome" pop-up window
-    with open('master.key', 'w') as master_file:
-        master_password_provided = simpledialog.askstring('Welcome :)', "Please set a master password.\n"
-                                                                        "This will be what you use to access all of "
-                                                                        "your other passwords, so don't forget it!")
-        saved_master_password = master_password_provided
-        master_file.write(master_password_provided)
+# keeps our window open until exited, so that we can successfully interact with our program
+try:
+    with open('master.key', 'r') as master_file:
+        master_key = master_file.read()
+except FileNotFoundError:
+    with open('salt.key', 'wb') as salt_file:
+        new_salt = secrets.token_urlsafe(16).encode()
+        salt_file.write(new_salt)
 
-window.mainloop()  # keeps our window open until exited, so that we can successfully interact with our program
+    with open('master.key', 'wb') as master_file:
+        password_provided = simpledialog.askstring('Welcome :)', "Please enter a master password.\n"
+                                                                 "This will be what you use to access all of your "
+                                                                 "other passwords, so don't forget it!")
+        password_encoded = password_provided.encode()  # convert entered pass to bytes
+
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=new_salt,
+            iterations=100000,
+            backend=default_backend(),
+        )
+
+        key = base64.urlsafe_b64encode(kdf.derive(password_encoded))  # can only use kdf once
+
+        master_file.write(key)  # writes our key to the master.key file.
+
+window.mainloop()
